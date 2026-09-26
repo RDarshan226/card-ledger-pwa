10eee1bf34480fee296fb269057f3303f00386dc
RDarshan226 <darshan.r015@proton.me>
2026-09-26 22:52:53 +0530

    Bump service worker cache to v31

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='card-ledger-pwa-v30';
+const CACHE='card-ledger-pwa-v31';
 const APP=['./','./index.html','./manifest.json','./sw.js','./chat-updates.json','./icon-192.svg','./icon-512.svg'];
 
 self.addEventListener('install',event=>{
