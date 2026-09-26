e0e7f43188ca16e55cf5b360ab623f1f01261442
RDarshan226 <darshan.r015@proton.me>
2026-09-26 21:23:18 +0530

    Bump PWA cache to v21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='card-ledger-pwa-v20';
+const CACHE='card-ledger-pwa-v21';
 const APP=['./','./index.html','./manifest.json','./sw.js','./chat-updates.json','./icon-192.svg','./icon-512.svg'];
 
 self.addEventListener('install',event=>{
